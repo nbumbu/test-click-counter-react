f72d1a45a1c3762129448d91df946273e80bad11
Nick <sahas023@gmail.com>
2019-09-25 13:42:23 +0200

    addd enzyme package

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Enzyme, {shallow} from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({adapter: new EnzymeAdapter()});
+
+test('renders without error', () => {
+
+});
+
+test('renders increment button', () => {
+
+});
+
+test('rendres counter display', () => {
+
+});
+
+test('counter starts at 0', () => {
+
+});
+
+test('increments counter on button click on display', () => {
+
 });
